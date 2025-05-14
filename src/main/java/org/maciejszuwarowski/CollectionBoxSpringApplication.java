3b80c8b49a0fef513598ffef9654f129ef076864
Maciej Szuwarowski <m9szuwarowski@interia.pl>
2025-05-14 16:38:46 +0200

    added depedencies, spring, mockmvc, h2 database. changed main class

---
+++ src/main/java/org/maciejszuwarowski/CollectionBoxSpringApplication.java
@@ -0,0 +1,12 @@
+package org.maciejszuwarowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionBoxSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionBoxSpringApplication.class, args);
+    }
+
+}
